d5ce5dff36238e10a0e72d6d43ef6458273e21b9
jessica <jessicadasilvaesilva8@gmail.com>
2025-08-27 20:08:32 -0300

    adicionado algumas referencias

--- calculadora de nota.py
+++ calculadora de nota.py
@@ -3,6 +3,7 @@ aluno = input("qual é seu nome:")
 nota1= float(input("digite primeira nota:"))
 nota2 =float(input("digite segunda nota:"))
 nota3 =float(input("digite terceira nota:"))
+nota4 =float(input("digite quarta nota:"))
 media = (nota1 + nota2 + nota3)/3
 if media >=7:
     print("resultado:aprovado")
